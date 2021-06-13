03f4c54eb5f1ab02bde5b325d6e60bfebe832b0e
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-13 19:36:39 -0300

    Criando formulario de edicao e pegando categoria a ser editada

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -49,4 +49,19 @@ router.post("/categories/delete", (req, res) => {
 
 })
 
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+            res.render("admin/categories/edit", {category: category})
+
+        }else{
+            res.redirect("admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("admin/categories");
+    })
+})
+
 module.exports = router;
