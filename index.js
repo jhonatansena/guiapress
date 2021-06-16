5c388d9ab3df5fb34a543ca5f6e33b20f9438ea5
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-16 15:11:48 -0300

    Pagina de leitura de artigo

--- index.js
+++ index.js
@@ -32,6 +32,24 @@ Article.findAll().then(articles => {
     res.render("index", {articles: articles});
 })
 
+app.get("/:slug", (req, res) => {
+    var slug = req.params.slug;
+
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            res.render("article", {article: article});
+        }else{
+            res.redirect("/");
+        }
+    }).catch(error => {
+        res.redirect("/");
+    })
+})
+
     
 })
 
