c3268067d0bae3c77141ac638fd469ad494177ba
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-18 19:44:08 -0300

    Tela para ediçao de categoria

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -62,6 +62,25 @@ router.post("/articles/delete", (req, res) => {
 
 })
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {article: article, categories: categories})
+            })
+            
+
+        }else{
+            res.redirect("admin/articles");
+        }
+    }).catch(erro => {
+        res.redirect("admin/articles");
+    })
+})
+
+
 
 
 module.exports = router
