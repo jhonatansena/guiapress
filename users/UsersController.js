9a1cbe3808d3f04360339b6d533afa1883ba3247
Jhonatan Sena <jcostasenna@hotmail.com>
2021-06-20 15:59:03 -0300

    Criado tela de usuario, geraçao de hash da senha com bcrypt e verificando se o email cadastrado ja existe

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("./User")
+const User = require("./User");
+const bcrypt = require('bcrypt');
 
-router.get('/admin/users/create', (req, res) => {
+router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create");
 })
+
+router.post("/users/create", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOnde({
+        where: {
+            email: email
+        }
+    }).then(user => {
+        if(email != undefined){
+
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+        User.create({
+            email: email,
+            password: hash
+
+        }).then(() => {
+            res.redirect("/")
+        }).catch((err) => {
+            res.redirect("/")
+        })
+
+        }else{
+            res.redirect("/admin/users/create")
+        }
+    })
+
+  
+
+});
 
 
 module.exports = router
